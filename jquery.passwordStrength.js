7d3b1414f419e3159808863a707de23ef1ee5b1c
Davis Zanetti Cabral <daviscabral@gmail.com>
2011-05-25 15:13:52 -0300

    Initial commit

---
+++ jquery.passwordStrength.js
@@ -0,0 +1,69 @@
+(function($){
+  var PasswordStrength = function(options, element) {  
+    var el   = $(element);    
+    var self = this;
+  	options  = $.extend({
+      container: '#password_strength',
+      classes: {
+        weak: 'password_weak',
+        medium: 'password_medium',
+        strong: 'password_strong'
+      },
+      messages: {
+        weak: "Weak Password",
+        medium: "Not so bad",
+        strong: "Great Password"
+      }
+    }, options || {});
+    
+    self.countChars = function(passwordText) {
+      var countResult = {
+        size: passwordText.length,
+        letters: 0,
+        numbers: 0,
+        otherChars: 0
+      }
+      for(var i=0;i<countResult.size;i++) {
+        var curChar = passwordText[i];
+        if (/[0-9]/.exec(curChar)) {
+          countResult.numbers += 1;
+        } else if (/[A-Za-z]/.exec(curChar)) {
+          countResult.letters += 1;
+        } else {
+          countResult.otherChars += 1;
+        }
+      }
+      return countResult;
+    }
+    
+    self.removeAllClasses = function(curContainer) {
+      curContainer.removeClass(options.classes.weak).removeClass(options.classes.medium).removeClass(options.classes.strong);
+    }
+    
+    el.keyup(function(d){
+      var countResult  = self.countChars($(this).attr("value"));
+      var curContainer = $(options.container);
+      self.removeAllClasses(curContainer);  
+      with(countResult) {
+        if (size >= 8 && otherChars >= 1) {
+          curContainer.addClass(options.classes.strong);
+          curContainer.text(options.messages.strong);
+        } else if (size >= 5 && letters >= 2 && numbers >= 2) {
+          curContainer.addClass(options.classes.medium);
+          curContainer.text(options.messages.medium);
+        } else {
+          curContainer.addClass(options.classes.weak);        
+          curContainer.text(options.messages.weak);        
+        }        
+      }
+    });
+  }
+  
+	$.fn.passwordStrength = function(options) {  
+		return this.each(function() {
+			var element = $(this);
+			var passwordStrength = new PasswordStrength(options, element);
+			element.data('passwordStrength', passwordStrength);
+		});
+  }
+})(jQuery);
